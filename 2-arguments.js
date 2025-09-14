e8d075c41394359fa8f6fc02ec5cb2fa707e9ea5
Derrick Owusu-Asare <aderrrick649@gmail.com>
2025-09-14 18:33:17 +0000

    Commit

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,11 @@
-const arg = process.argv[2];
+#!/usr/bin/node
 
-if (arg) {
-  console.log(arg);
-} else {
+const args = process.argv.slice(2); // grab only the arguments after the script name
+
+if (args[0] === undefined) {
   console.log("No argument");
+} else if (args[1] === undefined) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
 }
